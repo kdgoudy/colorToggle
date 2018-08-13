b0eafe770e48e496e71fef4d98af91832633b6dd
Koki <kgoudy64@gmail.com>
2018-08-13 08:42:23 -0500

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -1,1 +1,14 @@
 
+var button = document.querySelector("button");
+var isBlue = false;
+
+button.addEventListener("click", function(){
+  if(isBlue){
+   document.body.style.background = "white";
+   isBlue = false;
+   }else {
+   document.body.style.background = "blue";
+   isBlue = true;
+  }
+ 
+});
